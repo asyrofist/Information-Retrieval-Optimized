43785f0c83cae1145dfee1151eab05a26137e9f8
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-20 10:52:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,12 @@ if desc:
   histogram = st.sidebar.checkbox("histogram")
   korelasi = st.sidebar.checkbox("korelasi")
   if histogram:
+    st.subheader("Histogram Parameter")
     fig, ax = plt.subplots()
     ax.hist(hasil)
     st.pyplot(fig)
   elif korelasi:
+    st.subheader("Heatmap Correlation")
     fig, correlation = plt.subplots()
     correlation = heart_df.corr()
     sns.heatmap(correlation)
@@ -31,34 +33,39 @@ if desc:
     
 
 elif df_feature:
-  list_feature = st.sidebar.selectbox("Berdasarkan?", ['max', 'min', 'average'])
+  list_feature = st.selectbox("Berdasarkan?", ['max', 'min', 'average'])
   hasil = heart_df[heart_df.anaemia == heart_df.anaemia.list_feature()] # berdasarkan maximum
   st.write(hasil)
   st.sidebar.subheader("Evaluation Parameter")
   histogram = st.sidebar.checkbox("histogram")
   korelasi = st.sidebar.checkbox("korelasi")
   if histogram:
+    st.subheader("Histogram Parameter")
     fig, ax = plt.subplots()
     ax.hist(hasil)
     st.pyplot(fig)
   elif korelasi:
+    st.subheader("Heatmap Correlation")
+    fig, correlation = plt.subplots()
     correlation = heart_df.corr()
     sns.heatmap(correlation)
-    st.pyplot()
+    st.pyplot(fig)
     
 elif group_feature:
-  list_feature = st.sidebar.selectbox("Berdasarkan?", ['age	anaemia', 'creatinine_phosphokinase', 'diabetes', 'ejection_fraction', 'high_blood_pressure', 'platelets', 'serum_creatinine', 'serum_sodium', 'sex', 'smoking', 'time', 'DEATH_EVENT'])
-  data_feature = st.sidebar.selectbox("Berdasarkan?", ['age	anaemia', 'creatinine_phosphokinase', 'diabetes', 'ejection_fraction', 'high_blood_pressure', 'platelets', 'serum_creatinine', 'serum_sodium', 'sex', 'smoking', 'time', 'DEATH_EVENT'])
-  hasil = heart_df.groupby(data_feature).list_feature.describe() # berdasarkan group
+  list_feature = st.selectbox("Berdasarkan?", ['age', 'anaemia', 'creatinine_phosphokinase', 'diabetes', 'ejection_fraction', 'high_blood_pressure', 'platelets', 'serum_creatinine', 'serum_sodium', 'sex', 'smoking', 'time', 'DEATH_EVENT'])
+  hasil = heart_df.groupby('age').list_feature.describe() # berdasarkan group
   st.write(hasil)
   st.sidebar.subheader("Evaluation Parameter")
   histogram = st.sidebar.checkbox("histogram")
   korelasi = st.sidebar.checkbox("korelasi")
   if histogram:
+    st.subheader("Histogram Parameter")
     fig, ax = plt.subplots()
     ax.hist(hasil)
     st.pyplot(fig)
   elif korelasi:
+    st.subheader("Heatmap Correlation")
+    fig, correlation = plt.subplots()
     correlation = heart_df.corr()
     sns.heatmap(correlation)
-    st.plotly
+    st.pyplot(fig)
